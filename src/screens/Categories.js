4185bb502100b562800c6a48e5ab66d9617fb303
Salvador <45272829+salvariable@users.noreply.github.com>
2020-08-18 00:05:06 -0500

    Add and read data JSON from project

--- src/screens/Categories.js
+++ src/screens/Categories.js
@@ -1,12 +1,35 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Text} from 'react-native';
+import {categories} from '../assets/heyirys_covid.json';
+import ListElement from '../components/ListElement';
+import ActionButton from '../components/ActionButton';
 
 export default () => {
+  const renderImages = () => {
+    const elementsArray = Object.keys(categories).map((key) => categories[key]);
+    return elementsArray.map((element) => (
+      <ListElement value={element} key={element.order} />
+    ));
+  };
+
   return (
     <View
       style={{
         flex: 1,
         backgroundColor: '#f7f4ff',
-      }}></View>
+        justifyContent: 'space-around',
+      }}>
+      <View style={{margin: 16, justifyContent: 'space-between'}}>
+        <Text style={{fontSize: 24, fontWeight: 'bold'}}>Category</Text>
+        <Text style={{fontSize: 16}}>Select an option to continue</Text>
+      </View>
+
+      <View>{renderImages()}</View>
+
+      <View style={{flexDirection: 'row'}}>
+        <ActionButton version={'secondary'} />
+        <ActionButton version={'primary'} />
+      </View>
+    </View>
   );
 };
