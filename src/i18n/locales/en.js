75c11cc82938bd91a1d72ef4c437fdf5950eb0c5
Salvador <45272829+salvariable@users.noreply.github.com>
2020-08-19 10:44:24 -0500

    Add KeyboardManager, I18n library and translations

---
+++ src/i18n/locales/en.js
@@ -0,0 +1,14 @@
+export default {
+  categoryTitle: 'Category',
+  categorySubtitle: 'Select an option to continue',
+  buttonBack: 'Back',
+  buttonForward: 'Next',
+  subcategoriesTitle: 'Report Price Gouging',
+  subcategoriesSubtitle:
+    'Please select the type of product or service that is being sold at an exorbitant or excessive price.',
+  detailsDescription: 'Please provide a Brief Description of the issue.',
+  detailsInstruction: 'Type here, 200 characters or 29 words allowed.',
+  mapInputPlaceholder: 'Type your location',
+  mapFooterInfo:
+    'Please provide your approximate location. This information will be kept confidential',
+};
---
+++ src/i18n/locales/es.js
@@ -0,0 +1,15 @@
+export default {
+  categoryTitle: 'Categoría',
+  categorySubtitle: 'Selecciona una opción para continuar',
+  buttonBack: 'Atrás',
+  buttonForward: 'Siguiente',
+  subcategoriesTitle: 'Reportar Incremento de Precio',
+  subcategoriesSubtitle:
+    'Por favor selecciona el tipo de producto o servicio que está siendo vendido a un precio exorbitante o excesivo.',
+  detailsDescription:
+    'Por favor proporciona una breve descripción de la situación.',
+  detailsInstruction: 'Escribe aquí, hasta 200 caracteres o 29 palabras',
+  mapInputPlaceholder: 'Ingresa tu ubicación',
+  mapFooterInfo:
+    'Por favor proporciona tu ubicación aproximada. Esta información será confidencial',
+};
