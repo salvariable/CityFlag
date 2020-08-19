86e27c4f70ca74d641244d81fe40b2682de501e0
Salvador <45272829+salvariable@users.noreply.github.com>
2020-08-19 11:27:40 -0500

    Render list element in Categories and Subcategories screens

--- src/screens/Categories.js
+++ src/screens/Categories.js
@@ -3,12 +3,28 @@ import {View, Text} from 'react-native';
 import {categories} from '../assets/heyirys_covid.json';
 import ListElement from '../components/ListElement';
 import ActionButton from '../components/ActionButton';
+import I18n from '../i18n/i18n';
+import {withTheme} from 'react-native-paper';
 
-export default () => {
+export default withTheme(({navigation, theme}) => {
   const renderImages = () => {
     const elementsArray = Object.keys(categories).map((key) => categories[key]);
-    return elementsArray.map((element) => (
-      <ListElement value={element} key={element.order} />
+    const sortedElements = elementsArray.sort((a, b) =>
+      a.order.localeCompare(b.order),
+    );
+    return sortedElements.map((element) => (
+      <ListElement
+        value={element}
+        key={element.order}
+        navigation={navigation}
+        target={
+          element.icon && element.subcategories
+            ? 'Subcategories'
+            : !element.icon
+            ? 'Details'
+            : 'Categories'
+        }
+      />
     ));
   };
 
@@ -16,12 +32,14 @@ export default () => {
     <View
       style={{
         flex: 1,
-        backgroundColor: '#f7f4ff',
+        backgroundColor: theme.colors.primary,
         justifyContent: 'space-around',
       }}>
       <View style={{margin: 16, justifyContent: 'space-between'}}>
-        <Text style={{fontSize: 24, fontWeight: 'bold'}}>Category</Text>
-        <Text style={{fontSize: 16}}>Select an option to continue</Text>
+        <Text style={{fontSize: 24, fontWeight: 'bold'}}>
+          {I18n.t('categoryTitle')}
+        </Text>
+        <Text style={{fontSize: 16}}> {I18n.t('categorySubtitle')}</Text>
       </View>
 
       <View>{renderImages()}</View>
@@ -32,4 +50,4 @@ export default () => {
       </View>
     </View>
   );
-};
+});
---
+++ src/screens/Subcategories.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {View, ScrollView, Text} from 'react-native';
+import {withTheme} from 'react-native-paper';
+import ListElement from '../components/ListElement';
+import I18n from '../i18n/i18n';
+
+export default withTheme(({theme, navigation, route}) => {
+  const {subcategories} = route.params;
+
+  const renderSubcategories = () => {
+    const elementsArray = Object.keys(subcategories).map(
+      (key) => subcategories[key],
+    );
+
+    return elementsArray.map((element) => (
+      <ListElement
+        value={element}
+        key={element.order}
+        navigation={navigation}
+        target={'Details'}
+      />
+    ));
+  };
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: theme.colors.primary,
+        justifyContent: 'space-around',
+      }}>
+      <View style={{margin: 16, justifyContent: 'space-between'}}>
+        <Text style={{fontSize: 24, fontWeight: 'bold'}}>
+          {I18n.t('subcategoriesTitle')}
+        </Text>
+        <Text style={{fontSize: 16}}>{I18n.t('subcategoriesSubtitle')}</Text>
+      </View>
+      <ScrollView>{renderSubcategories()}</ScrollView>
+    </View>
+  );
+});
